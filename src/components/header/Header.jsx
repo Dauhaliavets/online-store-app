daec825fe0f5def2669f63d96af4536290ea4afc
Viachaslau <viachaslauihnatsiuk@gmail.com>
2022-03-31 12:45:53 +0300

    feat: add account component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import s from './Header.module.css';
 import logo from '../../assets/images/logo.png';
 import Form from '../search/Form';
+import Account from '../account/Account';
 
 
 
 const Header = () => {
 	return (
 		<div className={s.wrapper}>
-			<div className={s.logo}><img src={logo} alt="logo" /></div>
-			<Form />
+			<div className={s.main}>
+				<div className={s.logo}><img src={logo} alt="logo" /></div>
+				<Form />
+				<Account />
+			</div>
 		</div>
 	)
 }
