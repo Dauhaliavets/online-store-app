6dbb14b81b6e5a81d4eb21c374cb6f319f775a15
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-03-31 22:57:41 +0300

    refactor: test

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Header from './components/header/Header';
 import Main from './components/main/Main';
 import Footer from './components/footer/Footer';
 
-
-
 function App() {
 	return (
 		<div className={s.wrapper}>
