977f2aa709fa799ef19d8b57168141698c8c402d
Viachaslau <viachaslauihnatsiuk@gmail.com>
2022-04-01 12:22:34 +0300

    feat: add pop-up for registration/sign in

--- src/components/account/Account.jsx
+++ src/components/account/Account.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import s from './Account.module.css';
+import arrow from '../../assets/icons/arrow.svg';
 
 
 
 const Account = () => {
+	const [popup, setPopup] = useState(false)
+
 	return (
 		<div className={s.wrapper}>
-			<p className={s.greeting}>Hello, John!</p>
-			<p className={s.info}>Accounts & Lists</p>
+			<div className={s.main} onClick={() => setPopup(!popup)}>
+				<p className={s.greeting}>Hello, Sign in</p>
+				<p className={s.info}>Accounts & Lists</p>
+				<img className={s.arrow} src={arrow} alt="arrow" />
+			</div>
+			<div className={popup ? s.popup : s.popup__off}>
+				<div className={s.triangle}></div>
+				<button className={s.button}>Sign in</button>
+				<p className={s.text}>New customer? <button className={s.link}>Start here</button></p>
+			</div>
 		</div>
 	)
 }
