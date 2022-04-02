e409115d0fbf9c18753b41362b078141266b96b7
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-04-02 20:19:04 +0300

    feat: add store, cartReducer

--- src/components/basket/ShoppingCart/ShoppingCart.jsx
+++ src/components/basket/ShoppingCart/ShoppingCart.jsx
@@ -1,41 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import { getDataFromServer } from '../../../api/server';
+import { useSelector } from 'react-redux';
 import SubTotal from '../SubTotal/SubTotal';
 import Card from './Card/Card';
 import Preloader from './Preloader/Preloader';
 import styles from './ShoppingCart.module.css';
 
 export default function ShoppingCart() {
-	const [data, setData] = useState(null);
-	const [cards, setCards] = useState([]);
-	const [totalPrice, setTotalPrice] = useState(0);
+	const cart = useSelector(state => state.cart);
 
-	useEffect(() => {
-		getDataFromServer('products').then((data) => setData(data.splice(0, 2)));
-	}, []);
-	
-	useEffect(() => {
-		if(data) {
-			setCards(getCards(data));
-			setTotalPrice(getTotalPrice(data));
+	const totalPrice = cart.reduce((acc, cur) => {
+		if(cur.isChoise) {
+			acc = acc + +cur.price
+			return +(acc.toFixed(2))
 		}
-	}, [data])
-
-	const getCards = data => data.map((item, index) => <Card key={index} data={item}/>);
-	const getTotalPrice = data => data.reduce((acc, cur) => {
-		acc = acc + +cur.price
-		return +(acc.toFixed(2))
+		return acc;
 	}, 0)
 
 	return (
 		<div className={styles.wrapper}>
 			<h3 className={styles.title}>ShoppingCart</h3>
 			{
-				data 
-					? cards
-					: <Preloader />
+				cart.map((item, index) => <Card key={index} data={item}/>)
 			}
-			<SubTotal totalPrice={totalPrice} countItems={cards.length} />
+			<SubTotal totalPrice={totalPrice} countItems={cart.length} />
 		</div>
 	);
 }
