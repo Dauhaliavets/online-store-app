2857b872275555820a74111017ac311e34894c2f
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-04-04 15:11:02 +0300

    :refactor: minor refactor

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import s from './Main.module.css';
 import main from '../../assets/images/main.jpg';
-import ProductListing from './product_listing/ProductListing';
-import CartContainer from '.././cart/CartContainer/CartContainer';
+// import ProductListing from './product_listing/ProductListing';
+import CartContainer from '../cart/CartContainer/CartContainer';
 
 const Main = () => {
 	return (
 		<div className={s.wrapper}>
 			{<img src={main} alt="main" /> &&
+				/* <ProductListing /> */
 				<CartContainer />}
 		</div>
 	)
