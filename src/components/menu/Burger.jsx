b444f68a5c91e39ae5a615a513b7c7f4be65216f
semastep96 <semastep5@gmail.com>
2022-04-12 19:41:22 +0300

    add links in burger

--- src/components/menu/Burger.jsx
+++ src/components/menu/Burger.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import s from './Burger.module.css';
 import close from '../../assets/icons/close.svg';
+import { setFilter } from '../../redux-store/actions/productsActions';
+import { NavLink } from 'react-router-dom';
 
 const Burger = ({ menuToggle, setMenuToggle }) => {
+	const dispatch = useDispatch()
 	const user = useSelector(state => state.user);
+
+	const categories = [...new Set(useSelector(state => state.products.all.map(product => product.category)))].slice(0, 7)
+
+	const links = categories.map(categoryName => {
+		return (
+				<li className={s.link}>
+					<NavLink to='/products' key={categoryName} className={s.category}>
+						<div onClick={() => {
+							dispatch(setFilter({ category: categoryName }))
+							setMenuToggle(false)
+						}}>
+							{categoryName}
+						</div>
+					</NavLink>
+				</li>
+		);
+	})
+
 
 	return (
 		<div className={menuToggle ? s.active : s.menu} >
@@ -13,15 +34,17 @@ const Burger = ({ menuToggle, setMenuToggle }) => {
 			<h2 className={s.account}>Hello, {user.name ? user.name : 'Sign in'}</h2>
 			<nav className={s.body}>
 				<ul className={s.list}>
-					<li className={s.link}>Computer</li>
-					<li className={s.link}>Notebooks</li>
-					<li className={s.link}>Phones</li>
-					<li className={s.link}>Television</li>
-					<li className={s.link}>Accessories</li>
-					<li className={s.link}>Headphones</li>
-					<li className={s.link}>Home Audio</li>
-					<li className={s.link}>Office Electronics</li>
-					<li className={s.link}>Video Game Consoles</li>
+					<li className={s.link}>
+						<NavLink to='/products' className={s.category}>
+							<div onClick={() => {
+								dispatch(setFilter({ category: '' }))
+								setMenuToggle(false)
+							}}>
+								All
+							</div>
+						</NavLink>
+					</li>
+					{links}
 				</ul>
 			</nav>
 		</div>
