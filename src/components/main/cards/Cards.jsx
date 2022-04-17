125994d3f30185d51df292d0f7282ae7d330af0d
semastep96 <semastep5@gmail.com>
2022-04-17 16:14:28 +0300

    start page now work

--- src/components/main/cards/Cards.jsx
+++ src/components/main/cards/Cards.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import s from './Cards.module.css';
-import laptop from '../../../assets/images/cards/laptop.jpg';
-import computer from '../../../assets/images/cards/computer.jpg';
-import headphones from '../../../assets/images/cards/headphones.jpg';
-import phone from '../../../assets/images/cards/phone.jpg';
-import tablet from '../../../assets/images/cards/tablet.jpg';
-import watch from '../../../assets/images/cards/watch.jpg';
-import console from '../../../assets/images/cards/console.jpg';
-import accessories from '../../../assets/images/cards/accessories.jpg';
 import { Card } from './card/Card';
+import { useSelector } from 'react-redux';
 
 const Cards = () => {
+	const products = useSelector(state => state.products.all)
+	const categories = [...new Set (products.map(product => product.category))]
+
+	const cards = categories.map(category => {
+		const productWithImgId = products.findIndex(product => product.category === category)
+		const img = products[productWithImgId].img
+		return <Card image={img}
+						title={category}
+						key={category} />
+	})
+
 	return (
 		<div className={s.wrapper}>
-			<Card image={laptop} title='Laptops' />
-			<Card image={computer} title='Computers' />
-			<Card image={tablet} title='Tablets' />
-			<Card image={phone} title='Smartphones' />
-			<Card image={console} title='Video game consoles' />
-			<Card image={headphones} title='Headphones' />
-			<Card image={watch} title='Smart watches' />
-			<Card image={accessories} title='Accessories' />
+			{ cards }
 		</div>
 	)
 }
