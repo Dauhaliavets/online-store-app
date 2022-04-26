b3678688341e53cce5f5cb16ddff38a15408f04b
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-04-26 15:22:56 +0300

    fix: fix firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth, browserLocalPersistence, setPersistence } from "firebase/auth";
 
 const firebaseConfig = {
-	apiKey: process.env.REACT_APP_API_KEY,
-	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-	projectId: process.env.REACT_APP_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-	appId: process.env.REACT_APP_APP_ID,
-	measurementId: process.env.REACT_APP_MEASUREMENT_ID
+	apiKey: 'AIzaSyCuHxZgw7RqukU2sWyP-g-C7vHnxQHbV_g',
+	authDomain: 'online-store-app-28967.firebaseapp.com',
+	projectId: 'online-store-app-28967',
+	storageBucket: 'online-store-app-28967.appspot.com',
+	messagingSenderId: '241453082886',
+	appId: '1:241453082886:web:e4856716932569bf231687',
+	measurementId: 'G-5LXEB1GNLS'
 };
 
 const app = initializeApp(firebaseConfig);
